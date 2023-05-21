a50ae29bfcb4bf43438577341030ddbdf6e4e30d
shuja <53631362+shuja-waraich@users.noreply.github.com>
2023-05-21 18:08:30 +0500

    first commit

---
+++ knowledge_gpt/knowledge_gpt/components/sidebar.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+from components.faq import faq
+
+
+
+st.session_state["OPENAI_API_KEY"] = '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'
+
+
+def sidebar():
+    with st.sidebar:
+        pass
+    
+        
+        # set_openai_api_key('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')
+
