1ad74ae996f3a562daff3e4293d03a06fc586c49
shuja <53631362+shuja-waraich@users.noreply.github.com>
2023-05-22 02:46:35 +0500

    Update prompts.py

--- knowledge_gpt/knowledge_gpt/prompts.py
+++ knowledge_gpt/knowledge_gpt/prompts.py
@@ -1,21 +1,17 @@
 from langchain.prompts import PromptTemplate
 
 ## Use a shorter template to reduce the number of tokens in the prompt
-template = """Create a final answer to the given questions using the provided document excerpts(in no particular order) as references. ALWAYS include a "SOURCES" section in your answer including only the minimal set of sources needed to answer the question. If you don't uderstand the question, ask them to give more details.
-
+template = """ So for every question, there should be two or three answers. Even if the answer is,
+"to comply with weatherproofing there are three options, Deemed to Satisfy (H2D8), Performaance (H2P1) and the Verification method (H2V1). Which one would you like more information about?"
 ---------
 
-QUESTION: What  is the purpose of ARPA-H?
+QUESTION: How do i comply with the weatherproofing requirements, there are three answers. ?
 =========
-Content: More support for patients and families. \n\nTo get there, I call on Congress to fund ARPA-H, the Advanced Research Projects Agency for Health. \n\nIt’s based on DARPA—the Defense Department project that led to the Internet, GPS, and so much more.  \n\nARPA-H will have a singular purpose—to drive breakthroughs in cancer, Alzheimer’s, diabetes, and more.
-Source: 1-32
-Content: While we’re at it, let’s make sure every American can get the health care they need. \n\nWe’ve already made historic investments in health care. \n\nWe’ve made it easier for Americans to get the care they need, when they need it. \n\nWe’ve made it easier for Americans to get the treatments they need, when they need them. \n\nWe’ve made it easier for Americans to get the medications they need, when they need them.
-Source: 1-33
-Content: The V.A. is pioneering new ways of linking toxic exposures to disease, already helping  veterans get the care they deserve. \n\nWe need to extend that same care to all Americans. \n\nThat’s why I’m calling on Congress to pass legislation that would establish a national registry of toxic exposures, and provide health care and financial assistance to those affected.
-Source: 1-30
+For a Deemed to Satisfy compliance, you need to following the information in H2D8,
+For a Performance Pathway, you need to a report in line with A5G3 that addresses the performance requirements of H2P2
+Or You can also comply with the performance requirements (H2P2) by having a report prepared that meets the requirements of the Verification Methods (H2V1).
 =========
-FINAL ANSWER: The purpose of ARPA-H is to drive breakthroughs in cancer, Alzheimer’s, diabetes, and more.
-SOURCES: 1-32
+FINAL ANSWER: to comply with weatherproofing there are three options, Deemed to Satisfy (H2D8), Performaance (H2P1) and the Verification method (H2V1). Which one would you like more information about?
 
 ---------
 
