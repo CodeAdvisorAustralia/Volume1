4a0d57212db7ebde0349fcc0006e92d6c704718d
shuja <53631362+shuja-waraich@users.noreply.github.com>
2023-05-22 04:52:20 +0500

    Update prompts.py

--- knowledge_gpt/knowledge_gpt/prompts.py
+++ knowledge_gpt/knowledge_gpt/prompts.py
@@ -12,15 +12,12 @@ For a Deemed to Satisfy compliance, you need to following the information in H2D
 For a Performance Pathway, you need to a report in line with A5G3 that addresses the performance requirements of H2P2
 Or You can also comply with the performance requirements (H2P2) by having a report prepared that meets the requirements of the Verification Methods (H2V1).
 =========
-FINAL ANSWER: to comply with weatherproofing there are three options, Deemed to Satisfy (H2D8), Performaance (H2P1) and the Verification method (H2V1). Which one would you like more information about?
+ANSWER: to comply with weatherproofing there are three options, Deemed to Satisfy (H2D8), Performaance (H2P1) and the Verification method (H2V1). Which one would you like more information about?
 
 ---------
 
 QUESTION: {question}
-=========
-{summaries}
-=========
-FINAL ANSWER:"""
+:"""
 
 STUFF_PROMPT = PromptTemplate(
     template=template, input_variables=["summaries", "question"]
