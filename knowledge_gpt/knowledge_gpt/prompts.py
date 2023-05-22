c3cca9b7efaf05892c255f926db91768c22ddb01
shuja <53631362+shuja-waraich@users.noreply.github.com>
2023-05-22 06:23:50 +0500

    Update prompts.py

--- knowledge_gpt/knowledge_gpt/prompts.py
+++ knowledge_gpt/knowledge_gpt/prompts.py
@@ -1,23 +1,24 @@
-from langchain.prompts import PromptTemplate
-
-## Use a shorter template to reduce the number of tokens in the prompt
 template = """ 
-Never mention pages, instead highlight the relevant references, follow the requirements of the question closely
-So for every question, there should be two or three answers. Even if the answer is,
-"to comply with weatherproofing there are three options, Deemed to Satisfy (H2D8), Performaance (H2P1) and the Verification method (H2V1). Which one would you like more information about?"
+Never mention pages, instead highlight the relevant sections such as B1P1, follow the requirements of the question closely
 ---------
 
-QUESTION: How do i comply with the weatherproofing requirements, there are three answers. ?
+QUESTION: what clauses detail termite requirements?
 =========
-For a Deemed to Satisfy compliance, you need to following the information in H2D8,
-For a Performance Pathway, you need to a report in line with A5G3 that addresses the performance requirements of H2P2
-Or You can also comply with the performance requirements (H2P2) by having a report prepared that meets the requirements of the Verification Methods (H2V1).
-=========
-FINAL ANSWER: to comply with weatherproofing there are three options, Deemed to Satisfy (H2D8), Performaance (H2P1) and the Verification method (H2V1). Which one would you like more information about?
+FINAL ANSWER : Termite Actions are listed in B1P1(2)(o) as a method of satisfying compliance with 'B1P1 Structural Reliability' performance requirements
 
+Additionally, the provision B1D4 relates to the determination of the structural resistance of materials and forms of construction. Specifically, it addresses termite risk management for primary building elements. The following points summarize the requirements outlined in the provision:
+
+Primary building elements susceptible to subterranean termite attack must comply with AS 3660.1, a standard that provides guidelines for termite management.
+Certain materials are considered not subject to termite attack, including steel, aluminum, concrete, masonry, fibre-reinforced cement, timber that is naturally termite resistant (as specified in Appendix C of AS 3660.1), and timber that is preservative treated (as outlined in Appendix D of AS 3660.1).
+A durable notice must be permanently affixed to the building in a prominent location (such as the meter box) and include the following information:
+The termite management system used.
+The date of installation of the termite management system.
+For chemical treatments, the life expectancy of the pesticide as listed on the appropriate authority's pesticides register label.
+The recommendations from the installer or manufacturer regarding the scope and frequency of future inspections for termite activity.
+These requirements aim to ensure proper termite management and provide information for ongoing inspections and maintenance of the building.
 ---------
 
-QUESTION: {question}
+Question: {question}
 =========
 {summaries}
 =========
