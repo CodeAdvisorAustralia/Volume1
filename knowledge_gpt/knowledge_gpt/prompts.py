c00916a11fef33b8b66450273c65b182c34a5b1b
shuja <53631362+shuja-waraich@users.noreply.github.com>
2023-05-22 05:08:27 +0500

    Update prompts.py

--- knowledge_gpt/knowledge_gpt/prompts.py
+++ knowledge_gpt/knowledge_gpt/prompts.py
@@ -1,9 +1,8 @@
 from langchain.prompts import PromptTemplate
 
 ## Use a shorter template to reduce the number of tokens in the prompt
-template = """ So for every question, there should be two or three answers. Always give references to certain sections like the follwoing,
+template = """ So for every question, there should be two or three answers. Even if the answer is,
 "to comply with weatherproofing there are three options, Deemed to Satisfy (H2D8), Performaance (H2P1) and the Verification method (H2V1). Which one would you like more information about?"
-always ask the use what specific section are the looking for
 ---------
 
 QUESTION: How do i comply with the weatherproofing requirements, there are three answers. ?
@@ -12,12 +11,15 @@ For a Deemed to Satisfy compliance, you need to following the information in H2D
 For a Performance Pathway, you need to a report in line with A5G3 that addresses the performance requirements of H2P2
 Or You can also comply with the performance requirements (H2P2) by having a report prepared that meets the requirements of the Verification Methods (H2V1).
 =========
-ANSWER: to comply with weatherproofing there are three options, Deemed to Satisfy (H2D8), Performaance (H2P1) and the Verification method (H2V1). Which one would you like more information about?
+FINAL ANSWER: to comply with weatherproofing there are three options, Deemed to Satisfy (H2D8), Performaance (H2P1) and the Verification method (H2V1). Which one would you like more information about?
 
 ---------
 
 QUESTION: {question}
-:"""
+=========
+{summaries}
+=========
+FINAL ANSWER:"""
 
 STUFF_PROMPT = PromptTemplate(
     template=template, input_variables=["summaries", "question"]
