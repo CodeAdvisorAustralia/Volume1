f6d530178412daaaf1df1937dd736e6a48e85953
shuja <53631362+shuja-waraich@users.noreply.github.com>
2023-05-22 05:06:15 +0500

    Update main.py

--- knowledge_gpt/knowledge_gpt/main.py
+++ knowledge_gpt/knowledge_gpt/main.py
@@ -74,25 +74,25 @@ if button or st.session_state.get("submit"):
     else:
         st.session_state["submit"] = True
         # Output Columns
-        answer_col, sources_col = st.columns(2) 
-       
+        answer_col = st.empty()
+        sources = search_docs(index, query)
 
         try:
             answer = get_answer(sources, query)
-#             if not show_all_chunks:
-#                 # Get the sources for the answer
-#                 sources = get_sources(answer, sources)
+            # if not show_all_chunks:
+            #     # Get the sources for the answer
+            #     sources = get_sources(answer, sources)
 
             with answer_col:
                 st.markdown("#### Answer")
                 st.markdown(answer["output_text"].split("SOURCES: ")[0])
 
-            with sources_col:
-                st.markdown("#### Sources")
-                for source in sources:
-                    st.markdown(source.page_content)
-                    st.markdown(source.metadata["source"])
-                    st.markdown("---")
+            # with sources_col:
+            #     st.markdown("#### Sources")
+            #     for source in sources:
+            #         st.markdown(source.page_content)
+            #         st.markdown(source.metadata["source"])
+            #         st.markdown("---")
 
         except OpenAIError as e:
             st.error(e._message)
